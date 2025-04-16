fba3342e66ca98ef0e767c386524b00cafbb83ef
bhmberkan <berkanburakturgut@gmail.com>
2025-04-16 23:29:38 +0300

    Persistence katmanında dbcontextimizi oluşturduk. Service katmanında Create ve GetAllEvent metdolarımızı kodladık.

---
+++ Infrastructure/Eventify.Persistence/DbContexts/EventifyDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Eventify.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify.Persistence.DbContexts
+{
+    public class EventifyDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        // ms sql postegro sql gibi bir sql kullanmak yerine efcore un sunduğu ınmemory özelliğini kullandık.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("EventifyDb");
+        }
+    }
+}
