598d1235c1e91288fce793fe3ccaa312057388f0
bhmberkan <berkanburakturgut@gmail.com>
2025-04-16 20:39:28 +0300

    Domain Katmanı ile iglilendim. Event entitymi yazdım. daha sonra ortak olmasını düşündüğüm alanları (location) ValueObject olarak ayırıp kullanıma aldım. En sonda da common klasorunu oluşturup. EntityBase olarak her entityde yer almasını duşunduğum kısımları geçtim.

---
+++ Core/Eventify.Domain/Common/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify.Domain.Common
+{
+    public class EntityBase // <TKey> kullanarak guid cenerik yapilabilir 
+    {
+        
+      //  public TKey Id { get; set; }
+        public Guid Id { get; set; }
+
+        public DateTimeOffset CreateDate { get; set; }
+        public DateTimeOffset? UpdateDate { get; set; } // boş geçilebilir
+    }
+}
